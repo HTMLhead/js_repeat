3059b85edcbb4b8bf6eccda4ac83b106a17ecfe7
mrdoo <mrdoo309@gmail.com>
2018-08-03 14:20:18 +0900

    GUI게산기 2

---
+++ GUIcalculator1.js
@@ -0,0 +1,45 @@
+var input = {
+    'array': []
+};
+
+input.getInput = function() {
+    return this.array.join('');
+};
+
+var output = {};
+output.text = document.getElementById('output');
+
+var clickNumbers = function(event) {
+    var str = event.target.innerHTML;
+    
+    switch (str) {
+        case 'BS' :
+        input.array.pop();
+        break;
+        case '+' :
+        input.array.push(' ' + str + ' ');
+        break;
+        case '-' :
+        input.array.push(' ' + str + ' ');
+        break;
+        case '*' :
+        input.array.push(' ' + str + ' ');
+        break;
+        case '/' :
+        input.array.push(' ' + str + ' ');
+        break;
+        default :
+        input.array.push(str);
+    }
+
+    if (input.array.length === 0) {
+        output.text.innerHTML = 'Empty'
+    } else {
+        output.text.innerHTML = input.getInput();
+    }
+}
+
+var showResult = function(event) {
+    console.log('click other')
+    console.log(event.target.innerHTML);
+}
