6006530709d2015587a47ec05d3a38dbcb589e62
mrdoo <mrdoo309@gmail.com>
2018-08-07 14:08:12 +0900

    버튼동작, checking함수 구현
    swap, rpush, lpush 버튼 구혐
    checking은 첫줄의 문자열과 버튼의 문자열이 일치하지 않을때와 일치할 때를 구분시켜주는 함수

--- 4주차/puzzle.js
+++ 4주차/puzzle.js
@@ -1,20 +1,53 @@
 const str = document.getElementById('answer').innerHTML;
 const word = document.getElementById('word');
+const check = document.getElementById('check');
 
+var game = {};
+
+game.word = str.split('')
+game.btns = [];
 for (let i = 0; i < str.length; i++) {
-    const btn = document.createElement('button');
+    var btn = document.createElement('button');
     btn.innerHTML = str[i]
-    word.appendChild(btn);
+    word.appendChild(btn)
+    game.btns.push(btn)
+}
+
+const checking = function() {
+    if (game.word.join('') === str) {
+        check.innerHTML = '일치합니다.'
+    } else {
+        check.innerHTML = '일치하지 않습니다.'
+    }
+}
+
+const repeat = function() {
+    for (let i = 0; i < game.word.length; i++) {
+        game.btns[i].innerHTML = game.word[i]
+    }
 }
 
 const swap = function() {
-    console.log('swap');
+    let blank = [];
+    while(game.word.length != 0) {
+        var s = game.word.pop();
+        blank.push(s)
+    }
+    game.word = blank;
+    repeat();
+    checking();
 }
 
 const rpush = function() {
-    console.log('rpush');
+    let s = game.word.pop()
+    game.word.unshift(s)
+    repeat();
+    checking();
 }
 
 const lpush = function() {
-    console.log('lpush');
+    let s = game.word.shift()
+    game.word.push(s)
+    repeat();
+    checking();
 }
