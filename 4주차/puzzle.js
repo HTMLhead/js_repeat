af5bd398a3e4960fc8ee55f9bbdd3e64d74628ea
mrdoo <mrdoo309@gmail.com>
2018-08-08 10:09:08 +0900

    코드 리팩토링

--- 4주차/puzzle.js
+++ 4주차/puzzle.js
@@ -1,53 +1,95 @@
-const str = document.getElementById('answer').innerHTML;
-const word = document.getElementById('word');
-const check = document.getElementById('check');
+// puzzle
+// by mrdoo
+// 2018 .08 .08
 
-var game = {};
+// html element
+const output = document.getElementById('answer'); // answer
+const word = document.getElementById('word'); // display button
+const check = document.getElementById('check'); // checking
 
-game.word = str.split('')
-game.btns = [];
-for (let i = 0; i < str.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = str[i]
-    word.appendChild(btn)
-    game.btns.push(btn)
+const game = {
+    btns: [],
+};
+
+game.words = 'CRAZYBOY,NADOONADOO,SIMPSON,GGAMDOONGLEE,QUESTION'.split(',')
+game.choose = function () {
+    var idx = Math.floor(Math.random() * this.words.length);
+    this.answer = this.words[idx];
+    this.letters = this.answer.split('');
+    output.innerHTML = this.answer;
+};
+
+game.addButtons = function () {
+    for (let i = 0; i < this.letters.length; i++) {
+        let btn = document.createElement('button');
+        btn.innerHTML = this.letters[i]
+        word.appendChild(btn)
+        this.btns.push(btn)
+    }
 }
 
-const checking = function() {
-    if (game.word.join('') === str) {
-        check.innerHTML = '일치합니다.'
+game.checking = function () {
+    var gameStr = this.letters.join('')
+    if (gameStr === this.answer) {
+        check.innerHTML = '일치합니다'
     } else {
-        check.innerHTML = '일치하지 않습니다.'
+        check.innerHTML = '다릅니다.'
+    }
+};
+
+game.init= function() {
+    this.choose();
+    this.addButtons();
+    this.checking();
+}
+game.init();
+
+
+game.repeat = function () {
+    for (let i = 0; i < this.letters.length; i++) {
+        this.btns[i].innerHTML = this.letters[i]
     }
 }
 
-const repeat = function() {
-    for (let i = 0; i < game.word.length; i++) {
-        game.btns[i].innerHTML = game.word[i]
-    }
-}
-
-const swap = function() {
+// event Handler
+const swap = function () {
     let blank = [];
-    while(game.word.length != 0) {
-        var s = game.word.pop();
-        blank.push(s)
+    while (game.letters.length != 0) {
+        let s = game.letters.pop();
+        blank.push(s);
     }
-    game.word = blank;
-    repeat();
-    checking();
+
+    game.letters = blank;
+    game.repeat();
+    game.checking();
 }
 
-const rpush = function() {
-    let s = game.word.pop()
-    game.word.unshift(s)
-    repeat();
-    checking();
+const rpush = function () {
+    let s = game.letters.pop();
+    game.letters.unshift(s);
+    game.repeat();
+    game.checking();
 }
 
-const lpush = function() {
-    let s = game.word.shift()
-    game.word.push(s)
-    repeat();
-    checking();
+const lpush = function () {
+    let s = game.letters.shift();
+    game.letters.push(s);
+    game.repeat();
+    game.checking();
 }
+
+// shuffle
+game.shuffle = function () {
+    const toggle = Math.floor(Math.random() * 2) === 0;
+
+    if (toggle) {
+        swap();
+    }
+
+    const n = Math.floor(Math.random() * this.letters.length);
+
+    for (let i = 0; i < n; i++) {
+        lpush();
+    }
+}
+game.shuffle();
